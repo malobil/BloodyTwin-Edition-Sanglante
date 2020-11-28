572c9597b5cf1b73ec7ae9512601e6f3443030da
kevin.ibanezpro@gmail.com <kevin.ibanezpro@gmail.com>
2020-11-28 16:22:58 +0100

    re debug

--- Assets/Script/Characters/Character.cs
+++ Assets/Script/Characters/Character.cs
@@ -1,116 +1,113 @@
 ﻿using Photon.Pun;
 using Photon.Realtime;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public abstract class Character : MonoBehaviourPun
 {
-	[SerializeField] protected CharactersData m_characterData;
-	[SerializeField] protected Transform m_Camera;
-	[SerializeField] protected Camera m_CameraComp;
+    [SerializeField] protected CharactersData m_characterData;
+    [SerializeField] protected Transform m_Camera;
+    [SerializeField] protected Camera m_CameraComp;
 
-	protected GameControls m_controls;
-	protected Rigidbody m_rbComp;
-	protected Player m_associatePlayerInfo;
-	protected float m_xCameraRotation;
-	protected PhotonView m_photonView;
+    protected GameControls m_controls;
+    protected Rigidbody m_rbComp;
+    protected Player m_associatePlayerInfo;
+    protected float m_xCameraRotation;
+    protected PhotonView m_photonView;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		OnInit();
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnInit();
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		OnUpdate();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        OnUpdate();
+    }
 
-	public virtual void OnInit()
-	{
-		SetupPlayerInfos();
-		m_rbComp = GetComponent<Rigidbody>();
-		m_photonView = GetComponent<PhotonView>();
-		SetupControls();
-	}
+    public virtual void OnInit()
+    {
+        if (!photonView.IsMine)
+        {
+            m_CameraComp.gameObject.SetActive(false);
+            this.enabled = false;
+            return;
+        }
 
-	public virtual void OnUpdate()
-	{
-		Move();
-		CameraControl();
-	}
+        SetupPlayerInfos();
+        m_rbComp = GetComponent<Rigidbody>();
+        m_photonView = GetComponent<PhotonView>();
+        SetupControls();
+    }
 
-	public virtual void SetupControls()
-	{
-		m_controls = new GameControls();
-		EnableControls();
-	}
+    public virtual void OnUpdate()
+    {
+        Move();
+        CameraControl();
+    }
 
-	public virtual void EnableControls()
-	{
-		m_controls.Enable();
-	}
+    public virtual void SetupControls()
+    {
+        m_controls = new GameControls();
+        EnableControls();
+    }
 
-	public virtual void DisableControls()
-	{
-		m_controls.Disable();
-	}
+    public virtual void EnableControls()
+    {
+        m_controls.Enable();
+    }
 
-	public virtual void Move()
-	{
-		Vector2 inputs = m_controls.General.Move.ReadValue<Vector2>();
-		Vector3 movement = new Vector3(inputs.x, 0f, inputs.y);
-		movement = (movement.z * transform.forward * m_characterData.MoveSpeed) + (movement.x * transform.right *m_characterData.MoveSpeed) ;
-		movement.y = m_rbComp.velocity.y;
-		m_rbComp.velocity = movement;
-	}
+    public virtual void DisableControls()
+    {
+        m_controls.Disable();
+    }
 
-	public virtual void CameraControl()
-	{
-		float mouseX = m_controls.General.MouseDelta.ReadValue<Vector2>().x * m_characterData.CameraSensitivity ;
-		float mouseY = m_controls.General.MouseDelta.ReadValue<Vector2>().y * m_characterData.CameraSensitivity ;
-		transform.Rotate(Vector3.up * mouseX);
+    public virtual void Move()
+    {
+        Vector2 inputs = m_controls.General.Move.ReadValue<Vector2>();
+        Vector3 movement = new Vector3(inputs.x, 0f, inputs.y);
+        movement = (movement.z * transform.forward * m_characterData.MoveSpeed) + (movement.x * transform.right * m_characterData.MoveSpeed);
+        movement.y = m_rbComp.velocity.y;
+        m_rbComp.velocity = movement;
+    }
 
-		m_xCameraRotation -= mouseY;
-		m_xCameraRotation = Mathf.Clamp(m_xCameraRotation, -90f, 90f);
+    public virtual void CameraControl()
+    {
+        float mouseX = m_controls.General.MouseDelta.ReadValue<Vector2>().x * m_characterData.CameraSensitivity;
+        float mouseY = m_controls.General.MouseDelta.ReadValue<Vector2>().y * m_characterData.CameraSensitivity;
+        transform.Rotate(Vector3.up * mouseX);
 
-		m_Camera.localRotation = Quaternion.Euler(m_xCameraRotation, m_Camera.localRotation.y, m_Camera.localRotation.z);
-	}
+        m_xCameraRotation -= mouseY;
+        m_xCameraRotation = Mathf.Clamp(m_xCameraRotation, -90f, 90f);
 
-	public virtual void Interact()
-	{
-		RaycastHit hit;
+        m_Camera.localRotation = Quaternion.Euler(m_xCameraRotation, m_Camera.localRotation.y, m_Camera.localRotation.z);
+    }
 
-		if(Physics.Raycast(m_Camera.position,m_Camera.forward,out hit, m_characterData.range))
-		{
-			InteractableObject interactableHit = hit.transform.GetComponentInParent<InteractableObject>();
+    public virtual void Interact()
+    {
+        RaycastHit hit;
 
-			if (interactableHit != null)
-			{
-				interactableHit.OnInteract();
-			}
-		}
-	}
+        if (Physics.Raycast(m_Camera.position, m_Camera.forward, out hit, m_characterData.range))
+        {
+            InteractableObject interactableHit = hit.transform.GetComponentInParent<InteractableObject>();
 
-	public virtual void SetupPlayerInfos()
-	{
-		if(photonView.IsMine)
-		{
-			m_associatePlayerInfo = PhotonNetwork.LocalPlayer ;
-			Debug.Log(m_associatePlayerInfo.NickName);
-		}
-		else
-		{
-			m_CameraComp.gameObject.SetActive(false);
-			this.enabled = false;
-		}
-	}
+            if (interactableHit != null)
+            {
+                interactableHit.OnInteract();
+            }
+        }
+    }
 
-	public CharactersData GetDatas()
-	{
-		return m_characterData;
-	}
+    public virtual void SetupPlayerInfos()
+    {
+        m_associatePlayerInfo = PhotonNetwork.LocalPlayer;
+        Debug.Log(m_associatePlayerInfo.NickName);
+    }
+
+    public CharactersData GetDatas()
+    {
+        return m_characterData;
+    }
 }
